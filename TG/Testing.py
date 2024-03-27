7b10b13edeb0dea782f9ddf22927b285c481f811
JulianDPR <penaranda.julian@correounivalle.edu.co>
2024-03-27 17:04:30 -0500

    Antesde

--- TG/Testing.py
+++ TG/Testing.py
@@ -43,3 +43,64 @@ print(np.matrix(list(map(lambda x,y: Cn(pd.DataFrame(XY, columns = ["x","y"]),x,
 print(np.matrix(list(map(lambda x,y: np.vectorize(Cn__, excluded=[0])(pd.DataFrame(XY, columns = ["x","y"]),x,y), u, v))))
 
 
+#%%
+
+from tools import Bootst
+from tools import fitdist
+from tools import max_ll
+from tools import log_likelihood
+import scipy.optimize as so
+import scipy.stats as ss
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = "C:/Users/Bienvenido/Desktop/TG/Imag/gumbel_hougaard/muestra.xlsx"
+
+df = pd.read_excel(path, sheet_name = "strong")
+
+par = max_ll(df, ss.genextreme, [0.94, 0.23], log_likelihood)
+
+df["ranks"] = df.sort_values(by=0).reset_index().sort_values(by="index").reset_index()["level_0"]+1
+
+df["u"] = df["ranks"]/df.shape[0]
+
+df["f"] = ss.genextreme(0, *par).ppf(df["u"])
+
+df.columns = ["x"]+list(df.columns[1:])
+
+fig, ax = plt.subplots(figsize=(10,10))
+
+sns.regplot(df, x="x", y="f", line_kws = dict(color="darkred", alpha=1, linewidth=100),
+            scatter_kws=dict(color="darkcyan", alpha=0.8),
+            marker=".", ax=ax, ci = 95)
+
+ax.grid()
+
+#%%
+from scipy.optimize import ridder
+
+# Define la función que quieres minimizar con argumentos adicionales
+def funcion_a_minimizar(x, a, b):
+    return a * x ** 3 + b * x
+
+# Define los valores iniciales para los argumentos
+a_valores = [1, 2, 3]  # Varios valores para 'a'
+b_valor = 5  # Un único valor para 'b'
+
+# Función auxiliar para minimizar con argumentos adicionales fijos
+def funcion_auxiliar(x):
+    return funcion_a_minimizar(x, b=b_valor, a=a_valor)
+
+# Llama a la función ridder para encontrar el mínimo
+resultados = []
+for a_valor in a_valores:
+    resultado = ridder(funcion_auxiliar, -10, 10)
+    resultados.append(resultado)
+
+print("Los mínimos de la función para diferentes valores de 'a' son:", resultados)
+
+
+
+
+
